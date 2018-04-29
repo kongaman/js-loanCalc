9cc22c820421d56351ef5589d7d54e50557c08e0
Chris <gateway.kranz@gmail.com>
2018-04-29 22:35:19 +0200

    completed app

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Listen for Submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+    //Show loader
+    document.getElementById('loading').style.display = 'block';
+    // show spinner for 2 sec before showing results
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
 
 //Calculate Results
-function calculateResults(e){
+function calculateResults(){
     //UI Variables
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -16,17 +25,49 @@ function calculateResults(e){
     const calculatedPayments = parseFloat(years.value) * 12;
 
     // Compute monthly payment
-
     const x = Math.pow(1+ calculatedInterest, calculatedPayments);
     const monthly = (principal*x*calculatedInterest)/(x-1);
     
     if (isFinite(monthly)) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);    
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        //show results
+        document.getElementById('results').style.display = 'block';
+        //hide loader
+        document.getElementById('loading').style.display = 'none';    
     } else {
-        console.log('Please check your numbers');
+        showError('Please check your numbers.');
     }
+}
 
-    e.preventDefault();
+//Show error
+
+function showError(error) {
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //hide loader
+    document.getElementById('loading').style.display = 'none'; 
+
+    //create a div
+    const errorDiv = document.createElement('div');
+
+    // Get Elements to show error
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    //Add class
+    errorDiv.className ='alert alert-danger';
+    //Create Textnode and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+    //insert error above heading
+    //insert in card before heading
+    card.insertBefore(errorDiv, heading);
+
+    //Clear error after 3 Sec
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
 }
